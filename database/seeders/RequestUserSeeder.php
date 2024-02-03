089b9652f105e708190061d29a726b5b10b9a826
jldestive <jldestive2@gmail.com>
2024-02-03 14:21:38 -0500

    init seeder

--- database/seeders/RequestUserSeeder.php
+++ database/seeders/RequestUserSeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\Work;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class WorkSeeder extends Seeder
+class RequestUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Work::factory(10)->create();
+        //
     }
 }
